f268ce783dd9270afa14d54b45312691b5f52be2
Mark Coats <alexco1234@gmail.com>
2023-03-02 14:29:08 -0600

    fin

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  let list = movies.map(movie => {
+    return (
+      <div key={movie.title}>
+        
+        <h2>{movie.title}</h2>
+        <h3>{movie.time} minutes</h3>
+        <ul>
+          {movie.genres.map(genre => {
+            return <li key={genre}>{genre}</li>
+          })}
+        </ul>
+      </div>
+    )
+  })
+  return <div><h1>Movies Page</h1>{list}</div>;
 }
 
 export default Movies;
